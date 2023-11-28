fa6d919ec696ee6e385d330961d26d43573225bb
Marcos Vinicius <marcos.viniciuscruz00@gmail.com>
2023-11-28 07:45:06 -0300

    adicionei o metodo put

--- src/main/java/com/crud/simple/controller/ProductController.java
+++ src/main/java/com/crud/simple/controller/ProductController.java
@@ -3,10 +3,13 @@ package com.crud.simple.controller;
 import com.crud.simple.domain.product.Product;
 import com.crud.simple.domain.product.ProductRepository;
 import com.crud.simple.domain.product.RequestProductDTO;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -26,5 +29,21 @@ public class ProductController {
         Product newProduct = new Product(data);
         repository.save(newProduct);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProductDTO data){
+        Optional<Product> productOptional = repository.findById(data.id());
+        if (productOptional.isPresent()){
+            Product product = productOptional.get();
+            product.setName(data.name());
+            product.setDescription(data.description());
+            product.setPrice(data.price());
+            return ResponseEntity.ok(product);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
